ed5efd8fd406cad56a3220fd559fa89b4d16eee3
Amani-Daintree <amani@daintreetech.com>
2026-01-22 17:53:07 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,35 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php include("db.php")?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Checkout Page</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/footer.css">
 </head>
 <body>
-
 <footer class="main-footer">
     <section class="middle">
-    <h2>BRIGHTEN UP YOUR INBOX</h2><br>
-    <p>Join our email list for new product releases, endless inspiration, and 10% off your next order.</p><br>
-
+    <h2>GET 10% OFF STRAIGHT TO YOUR INBOX</h2><br>
+    <p>Join our mailing list to get early access to new arrivals and 10% off your first purchase.</p><br>
     <span class="error">email - can't be blank</span><br><br>
-
-  
 <div class="signup-wrap">
     <div class="email-box">
         <input type="email" placeholder="your-email@example.com">
         <button>SIGN UP</button>
     </div>
 </div>
+</section>
 <div class="custom-divider">
   <div class="accent-bar"></div>
 </div>
 <br>
 <br>
-</section>
-    <div class="content">
-    </div>
 
     <div class="footer-main-content">
         <div class="footer-col brand-col">
@@ -44,29 +39,27 @@
                 <p> Visakhapatnam, ZIP 530041</p>
             </div>
         </div>
-       
         <div class="footer-col">
-            <h3>Our collection</h3>
+            <h3 class="headings">Our collection</h3>
             <p>Home</p>
             <p>Blogs</p>
             <p>Shop</p>
             <p>Contact us</p>
             <p>About</p>
         </div>
-
         <div class="footer-col">
             <h3>Social media</h3>
             <ul>
                 <li>Instgram</li>
                 <li>Youtube</li>
-                <li>Pinterest</li>
-                <li>Twitter</li>
+                <!-- <li>Pinterest</li> -->
+                <!-- <li>Twitter</li> -->
                 <li>Facebook</li>
             </ul>
         </div>
         <div class="footer-col">
             <h3>Contact us</h3>
-            <p>Reach out is there any queries</p>
+            <p>  reach out  us for any queries</p>
             <form class="email-input-group">
                 <input type="email" placeholder="E-Mail" required>
                 <button type="submit" class="submit-btn">
@@ -77,10 +70,18 @@
     </div>
     <div class="footer-bottom">
         <div class="bottom-links">
-            Terms & Conditions | Privacy Policy | Data & Privacy | Return and Refund Policy | Shipping Policy
+            <a href="terms&condition">Terms & Conditions</a> |
+            <a href="privacy&policy.php">Privacy Policy</a> |
+            <a href="return&refundpolicy">Return and Refund Policy</a> |
+            <a href="shipping&policy">Shipping Policy</a>
             <br><br>
+
+
+
+
+
             </div>
-            <span class="copyright">Copyright &copy; 2024 waterberry</span>
+            <span class="copyright">Copyright &copy; 2025 waterberry</span>
         
         <div class="payment-icons">
             <img src="https://img.icons8.com/color/48/visa.png" alt="Visa">
